e9d886f621454786a1a9fcf7dceb3694a39cbbdb
nicklas hebsgaard <nicklashebsgaard@gmail.com>
2022-05-04 10:18:04 +0200

    Numbers

--- src/component/popularProcedures/PopularProcedures.jsx
+++ src/component/popularProcedures/PopularProcedures.jsx
@@ -13,6 +13,9 @@ import Two from "./../../assets/image/extra_procedures_etc/2.jpg";
 import Tree from "./../../assets/image/extra_procedures_etc/3.jpg";
 
 const PopularProcedures = () => {
+
+  const antal = 3;
+
   // STATE
 
   const [treatment, setTreatment] = useState();
@@ -48,39 +51,33 @@ const PopularProcedures = () => {
 
     <>
 
-      {treatment && (
+      <div className="wrapperPopular">
 
-        <>
+        <div className="textContainer">
+          <h2>Popular Procedures</h2>
+          <p>
+            To doesn't appear replenish together called he of mad place won't
+            wherein blessed second every wherein
+          </p>
+          <p>where meat kind wherein and martcin.</p>
+        </div>
 
-          {treatment.map(
+        <div className="cardContainer">
+          <>
+            {treatment &&
+              treatment.slice(0, antal).map(
+                (item) =>
+                  item && (
 
-            (item) =>
-              item && (
-
-                <div className="wrapperPopular">
-                    
-                  <div className="textContainer">
-                    <h2>Popular Procedures</h2>
-                    <p>
-                      To doesn't appear replenish together called he of mad
-                      place won't wherein blessed second every wherein
-                    </p>
-                    <p>where meat kind wherein and martcin.</p>
-                  </div>
-
-                  <div className="cardContainer">
                     <div className="popularCard">
+
                       <div className="imageHandler">
                         <img src={One} alt="Masseage Therapy" loading="lazy" />
                       </div>
 
                       <div className="textHandler">
-                        <h3>Masseage Therapy</h3>
-                        <p>
-                          Living winged said you darkness you're divide gathered
-                          and bring one seasons face great dr Waters firmament
-                          place which
-                        </p>
+                        <h3>{item.title}</h3>
+                        <p>{item.content}</p>
                       </div>
 
                       <div className="btnReadMore">
@@ -88,64 +85,18 @@ const PopularProcedures = () => {
                           <p>READ MORE</p>
                         </button>
                       </div>
+
                     </div>
 
-                    <div className="popularCard">
-                      <div className="imageHandler">
-                        <img src={Two} alt="Beauty Care" loading="lazy" />
-                      </div>
+                  )
 
-                      <div className="textHandler">
-                        <h3>Beauty Care</h3>
-                        <p>
-                          Living winged said you darkness you're divide gathered
-                          and bring one seasons face great dr Waters firmament
-                          place which
-                        </p>
-                      </div>
+              )}
 
-                      <div className="btnReadMore">
-                        <button>
-                          <p>READ MORE</p>
-                        </button>
-                      </div>
-                    </div>
+          </>
 
-                    <div className="popularCard">
-                      <div className="imageHandler">
-                        <img
-                          src={Tree}
-                          alt="Executive Reflexology"
-                          loading="lazy"
-                        />
-                      </div>
+        </div>
 
-                      <div className="textHandler">
-                        <h3>Executive Reflexology</h3>
-                        <p>
-                          Living winged said you darkness you're divide gathered
-                          and bring one seasons face great dr Waters firmament
-                          place which
-                        </p>
-                      </div>
-
-                      <div className="btnReadMore">
-                        <button>
-                          <p>READ MORE</p>
-                        </button>
-                      </div>
-                    </div>
-
-                  </div>
-                  
-                </div>
-              )
-
-          )}
-
-        </>
-
-      )}
+      </div>
 
       {loading && (
         <div>
